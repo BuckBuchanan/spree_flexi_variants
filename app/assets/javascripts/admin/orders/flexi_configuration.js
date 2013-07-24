f4f5b96828602ddb523e0731760644b058761d3f
BuckBuchanan <buck.buchanan@gmail.com>
2013-07-24 12:08:59 -0500

    Using less elegant JS that was working on my local box

--- app/assets/javascripts/admin/orders/flexi_configuration.js
+++ app/assets/javascripts/admin/orders/flexi_configuration.js
@@ -3,7 +3,7 @@ $.fn.product_autocomplete = function(){
   return this.each(function() {
     $(this).autocomplete({
       source: function(request, response) {
-        $.get(Spree.routes.product_search + '?' + jQuery.param({ q: $('.product_autocomplete').val(), authenticity_token: encodeURIComponent($('meta[name=csrf-token]').attr("content"))}), function(data) {
+        $.get("/admin/products.json?q=" + $('.product_autocomplete').val() + "&authenticity_token=" + encodeURIComponent($('meta[name=csrf-token]').attr("content")), function(data) {
           result = prep_product_autocomplete_data(data)
           response(result);
         });
